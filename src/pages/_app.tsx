99b72a505afeacc7e5010d016798177c5c051c20
Ana Laura <analaurahdn@gmail.com>
2022-06-27 02:14:47 -0300

    feat: title added to head.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import GlobalStyle from "../styles/globalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Ana Laura</title>
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
